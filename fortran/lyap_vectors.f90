a4f2c8af668c3b8d91a2a63d096e820a92904ffd
Jonathan Demaeyer <jodemaey@meteo.be>
2016-06-20 11:22:35 +0200

    Modified the routine to access the state of the Lyapunov calculation

--- fortran/lyap_vectors.f90
+++ fortran/lyap_vectors.f90
@@ -35,7 +35,7 @@ MODULE lyap_vectors
 
   PRIVATE
   
-  PUBLIC :: benettin_step,loclyap,lyapunov,ensemble,init_lyap,multiply_prop,get_prop
+  PUBLIC :: benettin_step,loclyap,lyapunov,ensemble,init_lyap,multiply_prop,get_lyap_state
  
   REAL(KIND=8), DIMENSION(:), ALLOCATABLE :: loclyap    !< Buffer containing the local Lyapunov exponent
   REAL(KIND=8), DIMENSION(:), ALLOCATABLE :: lyapunov   !< Buffer containing the averaged Lyapunov exponent
@@ -122,10 +122,11 @@ CONTAINS
    END SUBROUTINE benettin_step
 
    !> Routine that returns the current global propagator
-   SUBROUTINE get_prop(prop_ret)
-     REAL(KIND=8), DIMENSION(ndim,ndim),INTENT(OUT) :: prop_ret
+   SUBROUTINE get_lyap_state(prop_ret,ensemble_ret)
+     REAL(KIND=8), DIMENSION(ndim,ndim),INTENT(OUT) :: prop_ret,ensemble_ret
      prop_ret=prop
-   END SUBROUTINE get_prop
+     ensemble_ret=ensemble
+   END SUBROUTINE get_lyap_state
 
 END MODULE lyap_vectors
      
