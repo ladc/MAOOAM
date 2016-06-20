38e85b37363250376d1d31bb36a3eb5ff0c8d6b4
Sebastian Schubert <sebastian.schubert@uni-hamburg.de>
2016-06-20 22:14:59 +0200

    plot

--- fortran/plot_lyap_exp.m
+++ fortran/plot_lyap_exp.m
@@ -2,6 +2,9 @@
 % Short Plot Script for the Lyapunov exponents
 % run ./maooam_lyap to get file lyapunov_exponents.dat
 %
+clear all
+close all
+addpath(pathdef)
 
 %Read namelist
 nml=read_nml('params.nml','int_params.nml','modeselection.nml');
@@ -18,21 +21,45 @@ fid=fopen('lyapunov_exponents.dat');
 lle=fread(fid,ndim*n_len,'real*8');
 fclose(fid);
 lle=reshape(lle,ndim,n_len);
-
+lle_direct=importdata('lyapunov_exponents_div.dat');
+%
+[BLE_SV,VAR_BLE_SV,FLE_SV,VAR_FLE_SV,CLE_SV,VAR_CLE_SV] = read_SV;
 
 % unit of LEs in 1/day
 facLE=nml.F0*24*3600;
 
 % plot of spectrum
-figure;
-plot(mean(lle,2)*facLE);
+figLE=figure;
+subplot(2,1,1)
+plot(mean(lle,2)*facLE,'+');
+hold on;
+plot(BLE_SV,'.');
+plot(mean(lle_direct)*facLE,'o');
 title('Lyapunov Spectrum')
 xlabel('Lyapunov exponent number')
 ylabel('[1/day]')
 annotation(gcf,'textbox',...
-    [0.6 0.6 0.2 0.3],...
+    [0.175 0.6 0.2 0.178],...
     'String',{['DT=',num2str(nml.DT)],...
               ['T\_RUN=',num2str(nml.T_RUN)],...
               ['T\_TRANS=',num2str(nml.T_TRANS)],...
-              ['TW=',num2str(nml.TW)]},...
+              ['RESCALING_TIME=',num2str(nml.RESCALING_TIME)]},...
     'FitBoxToText','off','edgecolor','none');
+legend({'MAOOAM' 'Paper S&V' 'DIV'})
+subplot(2,1,2)
+plot(var(lle,[],2)*facLE^2,'+');
+hold on;
+plot(VAR_BLE_SV,'.');
+plot(var(lle_direct,[],1)*facLE^2,'o');
+title('VAR [1/day]')
+xlabel('Lyapunov exponent number')
+ylabel('[1/day]')
+legend({'MAOOAM' 'Paper S&V' 'DIV'})
+export_fig (figLE,'result_LE.png');
+
+figts=figure;
+plot(lle(1,:)*facLE);
+hold on;
+plot(lle_direct*facLE,'--')
+title('DIV vs QR LE 1')
+export_fig(figts,'result_ts.png');
