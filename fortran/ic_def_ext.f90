ee02e2443f46a4fbdd2edc32267f766899d09f3e
Sebastian Schubert <sebastian.schubert@uni-hamburg.de>
2016-08-22 15:57:27 +0200

    small bug fix in write_IC

--- fortran/ic_def_ext.f90
+++ fortran/ic_def_ext.f90
@@ -1,12 +1,13 @@
 
 ! ic_def_ext.f90
 !
-!>  Module to load the initial condition.
+!>  Module to load/write the initial condition.
 !
 !> @copyright                                                               
 !> 2016 Lesley De Cruz, Jonathan Demaeyer & Sebastian Schubert
 !> See LICENSE.txt for license information.                                  
 !> includes IC write routine
+!> includes loading specfic IC file
 !---------------------------------------------------------------------------!
 
 MODULE ic_def_ext
@@ -28,29 +29,33 @@ CONTAINS
 
   !> Subroutine to load the initial condition if IC.nml exists.
   !> If it does not, then write IC.nml with 0 as initial condition.
-  SUBROUTINE load_IC
-    INTEGER :: i,AllocStat,j
+  !> The optional argument filename allows to load an initial condition 
+  SUBROUTINE load_IC(ICname)
+    INTEGER :: i,AllocStat=0,j
     CHARACTER(len=20) :: fm
     REAL(KIND=8) :: size_of_random_noise
     INTEGER, DIMENSION(:), ALLOCATABLE :: seed
     CHARACTER(LEN=4) :: init_type 
+    CHARACTER(LEN=*), optional :: ICname
+    CHARACTER(LEN=:), ALLOCATABLE :: filename
     NAMELIST /IClist/ IC
     NAMELIST /RAND/ init_type,size_of_random_noise,seed
 
-
     fm(1:6)='(F3.1)'
-
+    
     CALL random_seed(size=j)
-     
     IF (ndim == 0) STOP "*** Number of dimensions is 0! ***"
-    ALLOCATE(IC(0:ndim),seed(j), STAT=AllocStat)
+    IF (.NOT. ALLOCATED(IC)) ALLOCATE(IC(0:ndim), STAT=AllocStat)
+    IF (.NOT. ALLOCATED(seed)) ALLOCATE(seed(j), STAT=AllocStat)
     IF (AllocStat /= 0) STOP "*** Not enough memory ! ***"
-
-    INQUIRE(FILE='./IC.nml',EXIST=exists)
+    
+    filename = 'IC'
+    IF (PRESENT(ICname)) filename = filename//'_'//ICname
+    INQUIRE(FILE='./'//filename//'.nml',EXIST=exists)
+    
     IC(0)=1.D0
-     
     IF (exists) THEN
-       OPEN(8, file="IC.nml", status='OLD', recl=80, delim='APOSTROPHE')
+       OPEN(8, file="./"//filename//".nml", status='OLD', recl=80, delim='APOSTROPHE')
        READ(8,nml=IClist)
        READ(8,nml=RAND)
        CLOSE(8)
@@ -61,7 +66,7 @@ CONTAINS
            IC=2*(IC-0.5)
            IC=IC*size_of_random_noise*10.D0
            IC(0)=1.0d0
-           WRITE(6,*) "*** IC.nml namelist written. Starting with 'seeded' random initial condition !***"
+           WRITE(6,*) "*** "//filename//".nml namelist written. Starting with 'seeded' random initial condition !***"
          CASE ('rand')
            CALL init_random_seed()
            CALL random_seed(get=seed)
@@ -69,19 +74,19 @@ CONTAINS
            IC=2*(IC-0.5)
            IC=IC*size_of_random_noise*10.D0
            IC(0)=1.0d0
-           WRITE(6,*) "*** IC.nml namelist written. Starting with random initial condition !***"
+           WRITE(6,*) "*** "//filename//".nml namelist written. Starting with random initial condition !***"
          CASE ('zero')
            CALL init_random_seed()
            CALL random_seed(get=seed)
            IC=0
            IC(0)=1.0d0
-           WRITE(6,*) "*** IC.nml namelist written. Starting with initial condition in IC.nml !***"
+           WRITE(6,*) "*** "//filename//".nml namelist written. Starting with initial condition in IC.nml !***"
          CASE ('read')
            CALL init_random_seed()
            CALL random_seed(get=seed)
            IC(0)=1.D0
            !nothing has to be done IC has already the right values
-           WRITE(6,*) "*** IC.nml namelist written. Starting with initial condition in IC.nml !***"
+           WRITE(6,*) "*** "//filename//".nml namelist written. Starting with initial condition in IC.nml !***"
        END SELECT
     ELSE
        CALL init_random_seed()
@@ -90,9 +95,9 @@ CONTAINS
        IC(0)=1.0D0
        init_type="zero"
        size_of_random_noise=0.D0
-       WRITE(6,*) "*** IC.nml namelist written. Starting with 0 as initial condition !***"
+       WRITE(6,*) "*** "//filename//".nml namelist written. Starting with 0 as initial condition !***"
     END IF
-    OPEN(8, file="IC.nml", status='REPLACE')
+    OPEN(8, file="./"//filename//".nml", status='REPLACE')
     WRITE(8,'(a)') "!------------------------------------------------------------------------------!"
     WRITE(8,'(a)') "! Namelist file :                                                              !"
     WRITE(8,'(a)') "! Initial condition.                                                           !"
@@ -151,11 +156,14 @@ CONTAINS
   SUBROUTINE write_IC(nameofcondition,field)
   CHARACTER(LEN=*) :: nameofcondition
   CHARACTER(LEN=20) :: fm
+  CHARACTER(LEN=:), allocatable :: filename
   REAL(KIND=8),dimension(0:ndim) :: field
   INTEGER :: i
     
     fm(1:6)='(F3.1)'
-    OPEN(8, file="IC_"//trim(nameofcondition)//".nml", status='REPLACE')
+    filename='IC'
+    IF (len(nameofcondition)>0) filename=filename//'_'//nameofcondition 
+    OPEN(8, file=filename//".nml", status='REPLACE')
     WRITE(8,'(a)') "!------------------------------------------------------------------------------!"
     WRITE(8,'(a)') "! Namelist file :                                                              !"
     WRITE(8,'(a)') "! Initial condition.                                                           !"
